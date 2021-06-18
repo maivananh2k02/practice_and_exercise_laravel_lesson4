cec49d8e147fb5f08d63627ce78fa6937c2d7e5b
thanhdeptrai <thanh.n5151@gmail.com>
2021-06-18 14:20:51 +0700

    thanh

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dataArray = [];
+        for ($i = 0; $i < 20; $i++) {
+            $data = [
+                array_push($dataArray, [
+                    'name' => Str::random(10),
+                    'password' => Str::random(5),
+//                    'dob' => date("Y-m-d", mt_rand(1, time())),
+                    'email' => Str::random(10) . '@gmail.com',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ])
+            ];
+        }
+        DB::table('users')->insert($dataArray);
+        //
+    }
+}
